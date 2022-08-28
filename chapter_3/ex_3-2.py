f2ef7cb19fac1e6096d8202b34354adb83d66892
mary-vien <maria.popach17@gmail.com>
2022-08-28 23:22:57 +0300

    edit ex_3-2

--- chapter_3/ex_3-2.py
+++ chapter_3/ex_3-2.py
@@ -3,7 +3,4 @@ message_0 = 'В квартирі №38 живе ' + names[0].title() + '.'
 message_1 = 'В квартирі №38 живе ' + names[1].title() + '.'
 message_2 = 'В квартирі №38 живе ' + names[2].title() + '.'
 message_3 = 'В квартирі №38 живе ' + names[3].title() + '.'
-print(message_0)
-print(message_1)
-print(message_2)
-print(message_3)
+print(message_0 + '\n' + message_1 + '\n' + message_2 + '\n' + message_3)
